d0e3233382dc54d9e9b0d9403a343d3820695cc2
allison <allisonyen24@berkeley.edu>
2025-03-16 12:56:50 -0700

    fad

--- PCU/Core/Src/FEB_Main.c
+++ PCU/Core/Src/FEB_Main.c
@@ -24,18 +24,22 @@ void FEB_Main_While(void){
 //
 	FEB_SM_ST_t bms_state = FEB_CAN_BMS_getState();
 
-	if (IS_TEST == false) {
+	if (IS_TEST == true) {
+		FEB_Normalized_updateAcc();
+		FEB_CAN_RMS_Process();
+	}
+
+	else { // not testing
 		if (FEB_Ready_To_Drive() && (bms_state == FEB_SM_ST_DRIVE || bms_state == FEB_SM_ST_DRIVE_REGEN)) {
 			FEB_Normalized_updateAcc();
 			FEB_CAN_RMS_Process();
+
 		} else {
 			FEB_Normalized_setAcc0();
 			FEB_CAN_RMS_Disable();
 		}
-	} else {
-		FEB_Normalized_updateAcc();
-		FEB_CAN_RMS_Process();
 	}
+
 	FEB_HECS_update();
 
 	FEB_CAN_RMS_Torque();
